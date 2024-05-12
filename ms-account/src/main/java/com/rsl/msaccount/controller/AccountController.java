26a57f7eb4a21be34358942ed5600aabe7f39cd2
imranrasulzade <imranrasulzade5@gmail.com>
2024-05-12 17:57:49 +0400

    load balancer configured for ms-account

--- ms-account/src/main/java/com/rsl/msaccount/controller/AccountController.java
+++ ms-account/src/main/java/com/rsl/msaccount/controller/AccountController.java
@@ -1,5 +1,6 @@
 package com.rsl.msaccount.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/account")
 public class AccountController {
+    private final Environment environment;
+
+    public AccountController(Environment environment) {
+        this.environment = environment;
+    }
+
 
     @GetMapping
-    public String get(){
-        return "Hello from MS-ACCOUNT";
+    public String getHelloAndPORT(){
+        return "Hello from MS-ACCOUNT\n" +
+                "Port number: " + environment.getProperty("local.server.port");
     }
 }
