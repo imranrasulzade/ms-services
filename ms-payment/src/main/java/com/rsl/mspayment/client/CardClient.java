763a85da6554c75b0f4e899fef3463ef02c863a8
imranrasulzade <imranrasulzade5@gmail.com>
2024-05-13 16:15:52 +0400

    test

--- ms-payment/src/main/java/com/rsl/mspayment/client/CardClient.java
+++ ms-payment/src/main/java/com/rsl/mspayment/client/CardClient.java
@@ -1,19 +1,13 @@
 package com.rsl.mspayment.client;
 
-import com.rsl.mspayment.exception.ExceptionResponseFeign;
-import com.rsl.mspayment.model.ExceptionConstants;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @FeignClient(name = "ms-card", url = "${client.ms-card.endpoint}")
 public interface CardClient {
 
-    @GetMapping("/api/cards")
-    List<CardDto> getAllAvailable(@RequestParam String currency);
+    List<CardDto> getAllAvailable(String currency);
 
 
 
